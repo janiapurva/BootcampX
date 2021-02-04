3f648d6ba5bc5168ff7238bb20027cc136e2abc5
janiapurva <jani.apurva@yahoo.com>
2021-02-03 20:02:43 -0500

    added student.js and teacchers.js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,30 @@
+//importing pool
+const { Pool } = require("pg");
+
+// connecting with database and localhost
+const pool = new Pool({
+  user: "apurvajani",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+pool
+  .query(
+    `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+ORDER BY teacher;
+`
+  )
+  .then((res) => {
+    res.rowsforEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  })
+  .catch(err =>
+    console.error('query error', err.stack));
